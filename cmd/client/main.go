4258b94a3a65d11e1bca3cea4b1ce4fe4dea1c7f
vladomdev <vlado.m.dev@gmail.com>
2024-12-17 01:17:31 +0100

    Configured Client REPL

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -3,8 +3,6 @@ package main
 import (
 	"fmt"
 	"log"
-	"os"
-	"os/signal"
 
 	amqp "github.com/rabbitmq/amqp091-go"
 	"github.com/vladomdev/learn-pub-sub-starter/internal/gamelogic"
@@ -41,11 +39,54 @@ func main() {
 		log.Fatalf("An error occured while Declaring and binding a Qeue: %v", err)
 	}
 
-	// wait for ctrl+c
-	sigChan := make(chan os.Signal, 1)
-	signal.Notify(sigChan, os.Interrupt)
-	<-sigChan
-	signal.Stop(sigChan)
-	close(sigChan)
-	fmt.Println("\nDisconnecting from Peril server...")
+	gameState := gamelogic.NewGameState(loggedUser)
+
+	for {
+		userInput := gamelogic.GetInput()
+		if len(userInput) == 0 {
+			continue
+		}
+		if userInput[0] == "spawn" {
+			err = gameState.CommandSpawn(userInput)
+			if err != nil {
+				fmt.Printf("An error occured while spawning %v's units: %v", loggedUser, err)
+			}
+			continue
+		}
+		if userInput[0] == "move" {
+			_, err = gameState.CommandMove(userInput)
+			if err != nil {
+				fmt.Printf("An error occured while moving %v's units: %v", loggedUser, err)
+			}
+			continue
+		}
+		if userInput[0] == "status" {
+			gameState.CommandStatus()
+			continue
+		}
+		if userInput[0] == "help" {
+			gamelogic.PrintClientHelp()
+			continue
+		}
+		if userInput[0] == "spam" {
+			fmt.Println("Spamming not allowed yet!")
+			continue
+		}
+		if userInput[0] == "quit" {
+			gamelogic.PrintQuit()
+			break
+		}
+		fmt.Println("\nCommand not understood. Please enter a valid command. Use 'help' to list valid commands.")
+		continue
+	}
+
+	/*
+		// wait for ctrl+c
+		sigChan := make(chan os.Signal, 1)
+		signal.Notify(sigChan, os.Interrupt)
+		<-sigChan
+		signal.Stop(sigChan)
+		close(sigChan)
+		fmt.Println("\nDisconnecting from Peril server...")
+	*/
 }
